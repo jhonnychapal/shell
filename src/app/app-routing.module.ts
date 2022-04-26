14870cf03891fc9e86e7e525e3cdfa7cd00ce621
jchapal <jchapal@bancolombia.com.co>
2022-04-26 15:46:28 -0500

    Module federation implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: ()=> import('./components/dashboard/dashboard.module').then((m) => m.DashboardModule)
+  },
+  {
+    path: 'producto1',
+    loadChildren: ()=> import('producto1/Producto1Module').then((m) => m.Producto1Module)
+  },
+  {
+    path: 'producto2',
+    loadChildren: ()=> import('producto2/Producto2Module').then((m) => m.Producto2Module)
   }
 ];
 
---
+++ src/app/decl.d.ts
@@ -0,0 +1,2 @@
+declare module 'producto1/Producto1Module'
+declare module 'producto2/Producto2Module'
